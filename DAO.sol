6294d1f98ed61ded6f7a5c844f3a499fab1fa967
CJentzsch <jentzsch.software@gmail.com>
2016-01-19 11:13:27 +0100

    fix double burning

--- DAO.sol
+++ DAO.sol
@@ -243,9 +243,9 @@ contract DAO is DAOInterface, Token, Crowdfunding {
         Proposal p = proposals[_proposalNumber];
         // sanity check
         if (now < p.votingDeadline  // has the voting deadline arrived?
+            || now > p.votingDeadline + 41 days
             || p.proposalHash != sha3(_newServiceProvider, 0, 0) // Does the transaction code match the proposal?
             || !p.newServiceProvider // is it a new service provider proposal
-           // || msg.sender == address(p.newDAO) // the new DAO can not call this function for various reasons (burn tokens, receive tokens, ...) NEEDS FIXING
             || p.recipient != _newServiceProvider) //(not needed)
             throw;
 
