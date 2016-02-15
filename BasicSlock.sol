5cb074f9821e869f0a99ea19000ff0fa244e49c6
CJentzsch <jentzsch.software@gmail.com>
2016-02-15 14:58:25 +0100

    compile fix

--- BasicSlock.sol
+++ BasicSlock.sol
@@ -3,7 +3,7 @@ import "./SubUser.sol";
 
 contract SlockitDAO {
     function payOneTimeFee() returns(bool) {}
-    function feeDivisor() const returns(uint) {}
+    function feeDivisor() constant returns(uint) {}
 }
 
 contract Slock is SubUser {
