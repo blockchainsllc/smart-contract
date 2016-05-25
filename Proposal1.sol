11b3d667aaed01dfa80908f8656c1b28421015d2
Lefteris Karapetsas <lefteris@refu.co>
2016-05-25 22:53:47 +0200

    Add SampleOffers to this repo

---
+++ Proposal1.sol
@@ -0,0 +1,79 @@
+/*
+This file is part of the DAO.
+
+The DAO is free software: you can redistribute it and/or modify
+it under the terms of the GNU lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The DAO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU lesser General Public License for more details.
+
+You should have received a copy of the GNU lesser General Public License
+along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+  Sample Proposal from a Contractor to the DAO including a reward towards the
+  DAO.
+
+  Feel free to use as a template for your own proposal.
+*/
+
+import "./Proposal2.sol";
+
+contract SampleOffer is SampleOfferWithoutReward {
+
+    uint public rewardDivisor;
+    uint public deploymentReward;
+
+        function SampleOffer(
+            address _contractor,
+            address _client,
+            bytes32 _IPFSHashOfTheProposalDocument,
+            uint _totalCosts,
+            uint _oneTimeCosts,
+            uint128 _minDailyWithdrawLimit
+        ) SampleOfferWithoutReward(
+            _contractor,
+            _client,
+            _IPFSHashOfTheProposalDocument,
+            _totalCosts,
+            _oneTimeCosts,
+            _minDailyWithdrawLimit) {
+        }
+
+    // interface for Ethereum Computer
+    function payOneTimeReward() returns(bool) {
+        // client DAO should not be able to pay itself generating
+        // "free" reward tokens
+        if (msg.sender == address(client) || msg.sender == address(originalClient))
+            throw;
+
+        if (msg.value < deploymentReward)
+            throw;
+
+        if (originalClient.DAOrewardAccount().call.value(msg.value)()) {
+            return true;
+        } else {
+            throw;
+        }
+    }
+
+    // pay reward
+    function payReward() returns(bool) {
+        // client DAO should not be able to pay itself generating
+        // "free" reward tokens
+        if (msg.sender == address(client) || msg.sender == address(originalClient))
+            throw;
+
+        if (originalClient.DAOrewardAccount().call.value(msg.value)()) {
+            return true;
+        } else {
+            throw;
+        }
+    }
+}
---
+++ Proposal2.sol
@@ -0,0 +1,154 @@
+/*
+This file is part of the DAO.
+
+The DAO is free software: you can redistribute it and/or modify
+it under the terms of the GNU lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+The DAO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU lesser General Public License for more details.
+
+You should have received a copy of the GNU lesser General Public License
+along with the DAO.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+  An Offer from a Contractor to the DAO without any reward going back to
+  the DAO.
+
+  Feel free to use as a template for your own proposal.
+
+  Actors:
+  - Offerer:    the entity that creates the Offer. Usually it is the initial
+                Contractor.
+  - Contractor: the entity that has rights to withdraw money to perform
+                its project.
+  - Client:     the DAO that gives money to the Contractor. It signs off
+                the Offer, can adjust daily withdraw limit or even fire the
+                Contractor.
+*/
+
+// TODO: Would need to either move the DAO contracts in this repo too, or
+// use this http://solidity.readthedocs.io/en/latest/layout-of-source-files.html#use-in-actual-compilers
+// solidity feature in order to point to their location
+import "./DAO.sol";
+
+contract SampleOfferWithoutReward {
+
+    // The total cost of the Offer. Exactly this amount is transfered from the
+    // Client to the Offer contract when the Offer is signed by the Client.
+    // Set once by the Offerer.
+    uint public totalCosts;
+
+    // Initial withdraw to the Contractor. It is done the moment the Offer is
+    // signed.
+    // Set once by the Offerer.
+    uint public oneTimeCosts;
+
+    // The minimal daily withdraw limit that the Contractor accepts.
+    // Set once by the Offerer.
+    uint128 public minDailyWithdrawLimit;
+
+    // The amount of money the Contractor has right to withdraw daily above the
+    // initial withdraw. The Contractor does not have to do the withdraws every
+    // day as this amount accumulates.
+    uint128 public dailyWithdrawLimit;
+
+    // The address of the Contractor.
+    address public contractor;
+
+    // The address of the Proposal/Offer document.
+    bytes32 public IPFSHashOfTheProposalDocument;
+
+    // The time of the last withdraw to the Contractor.
+    uint public lastPayment;
+
+    uint public dateOfSignature;
+    DAO public client; // address of DAO
+    DAO public originalClient; // address of DAO who signed the contract
+    bool public isContractValid;
+
+    modifier onlyClient {
+        if (msg.sender != address(client))
+            throw;
+        _
+    }
+
+    // Prevents methods from perfoming any value transfer
+    modifier noEther() {if (msg.value > 0) throw; _}
+
+    function SampleOfferWithoutReward(
+        address _contractor,
+        address _client,
+        bytes32 _IPFSHashOfTheProposalDocument,
+        uint _totalCosts,
+        uint _oneTimeCosts,
+        uint128 _minDailyWithdrawLimit
+    ) {
+        contractor = _contractor;
+        originalClient = DAO(_client);
+        client = DAO(_client);
+        IPFSHashOfTheProposalDocument = _IPFSHashOfTheProposalDocument;
+        totalCosts = _totalCosts;
+        oneTimeCosts = _oneTimeCosts;
+        minDailyWithdrawLimit = _minDailyWithdrawLimit;
+        dailyWithdrawLimit = _minDailyWithdrawLimit;
+    }
+
+    function sign() {
+        if (msg.sender != address(originalClient) // no good samaritans give us money
+            || msg.value != totalCosts    // no under/over payment
+            || dateOfSignature != 0)      // don't sign twice
+            throw;
+        if (!contractor.send(oneTimeCosts))
+            throw;
+        dateOfSignature = now;
+        isContractValid = true;
+        lastPayment = now;
+    }
+
+    function setDailyWithdrawLimit(uint128 _dailyWithdrawLimit) onlyClient noEther {
+        if (_dailyWithdrawLimit >= minDailyWithdrawLimit)
+            dailyWithdrawLimit = _dailyWithdrawLimit;
+    }
+
+    // "fire the contractor"
+    function returnRemainingEther() onlyClient {
+        if (originalClient.DAOrewardAccount().call.value(this.balance)())
+            isContractValid = false;
+    }
+
+    // Withdraw to the Contractor.
+    //
+    // Withdraw the amount of money the Contractor has right to according to
+    // the current withdraw limit.
+    // Executing this function before the Offer is signed off by the Client
+    // makes no sense as this contract has no money.
+    function getDailyPayment() noEther {
+        if (msg.sender != contractor)
+            throw;
+        uint timeSinceLastPayment = now - lastPayment;
+        // Calculate the amount using 1 second precision.
+        uint amount = (timeSinceLastPayment * dailyWithdrawLimit) / (1 days);
+        if (amount > this.balance) {
+            amount = this.balance;
+        }
+        if (contractor.send(amount))
+            lastPayment = now;
+    }
+
+    // Change the client DAO by giving the new DAO's address
+    // warning: The new DAO must come either from a split of the original
+    // DAO or an update via `newContract()` so that it can claim rewards
+    function updateClientAddress(DAO _newClient) onlyClient noEther {
+        client = _newClient;
+    }
+
+    function () {
+        throw; // this is a business contract, no donations
+    }
+}
